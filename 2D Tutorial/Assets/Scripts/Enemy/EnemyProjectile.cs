2e7f3b9b89c9d57c48fd74010c53b938ad78434c
Your Name <you@example.com>
2021-09-23 13:48:00 +0700

    first commit

---
+++ 2D Tutorial/Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyProjectile : EnemyDamage//kế thừa từ enemydamage
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float resetTime;
+    private float lifetime;
+
+    public void ActivateProjectile()
+    {
+        lifetime = 0;
+        gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > resetTime)
+            gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);//execute logic from parent script first
+        gameObject.SetActive(false);//Chạm vào vật cản sẽ hủy mũi tên
+    }
+}
---
+++ 2D Tutorial/Assets/Scripts/Enemy/Firetrap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFiretrap());
+
+            if(active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+    private IEnumerator ActivateFiretrap()
+    {
+        //Chuyen mau de biet da kich hoat bay
+        triggered = true;
+        spriteRend.color = Color.red;
+
+        //chờ độ trễ , kích hoạt bẫy, bật ảnh họa , màu trơ lại bình thường 
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white;//Trở lại màu ban đầu
+        active = true;
+        anim.SetBool("activated", true);
+
+
+        //hủy kích hoạt bẫy, tất cả trở lại ban đầu
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
